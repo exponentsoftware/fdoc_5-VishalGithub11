c69ac40d4f690f4296a830f61a50cbdb5db199f2
Vishal Modanwal <modanwalvishal@gmail.com>
2021-07-31 16:59:33 +0530

    countries languages and area

--- Assignment-02/NumOfLanguages.js
+++ Assignment-02/NumOfLanguages.js
@@ -8,60 +8,65 @@ const getData = async () => {
     const url = "https://restcountries.eu/rest/v2/all"
     const arr = [];
     const arr2 = [];
-    const arr3 = [];
     const res = await axios.get(url)
-    recdata = res.data
-    //  console.log(recdata);
-    let languageLength;
+  recdata = res.data
+  
+  let languageLength;
+  
+  //All countries and their languages
     recdata.map((item, index) => {
+      const countryName = item.name;
+      const bhasha = item.languages.map((language, index) =>language.name);
+      const numOfBhasha = bhasha.length;
 
-     
-        const countryName = item.name
-        const bhasha = item.languages.map((language) => language.name)
-        const numOfBhasha = bhasha.length
+       const output= countryName + " has " + numOfBhasha + " languages, i.e. " + bhasha
+        console.log(output);
+    });
 
-        // console.log(countryName + " has " + numOfBhasha + " languages, i.e. " + bhasha);        
-    })
+   //Top 15 countries with largest area
+
+    const getAllCountriesArea = () => {
+      const sorted = recdata.sort((a, b) => b.area - a.area).slice(0, 15);
+      sorted.map((item, index) => {
+        const name = item.name;
+        const area = item.area
+        console.log(name + ", area : " + area + " sq.km");
+      })
+    };
+    getAllCountriesArea();
 
 
  
+  // to find specific language spoken in different countries
+   const getLanguage = (way) => {
+      recdata.map((item, index) => {
+        const bhasha = item.languages.map((language) => language.name);
+        languageLength = bhasha.filter((word) => word == way).length;
+        arr.push(languageLength);
+      });
+      const SumOfALanguage = arr.reduce((a, b) => a + b);
 
-    
-   
+      console.log("language: " + way + ", spoken in " + SumOfALanguage + " countries");
+    };
+    getLanguage("Spanish");
+  
+  
+  // to find all languages in an array
+  const getAllLaunguage = () => {
+            recdata.map((item, index) => {
+                const bhasha = item.languages.map((language) => language.name)
+                arr2.push(bhasha)
+            });
 
-    const getLanguage =(way)=>{
-    recdata.map((item, index) => {
-        const bhasha = item.languages.map((language) => language.name)
-        languageLength = bhasha.filter((word) => word == way).length
-        arr.push(languageLength)
-        // console.log(arr);
+    arr2.map((item, index) => {
+      const newArray = [...item]
+      console.log(newArray);
+      return newArray
     })
-    const SumOfALanguage = arr.reduce((a,b)=> a+b)
-     
-        console.log("language: " + way + ", spoken in " + SumOfALanguage + " countries");
-    }
-    getLanguage("Spanish")
-
-
-
-
-
-
-
-    const getAllCountries = () => {
-        recdata.map((item, index) => {
-            const area = item.area;
-            const output = item.name + " have area of " + item.area + " sq.km"
-            // console.log(output)
-            arr3.push(area)
-        })
-        const values = arr3.sort((a,b) => b-a).slice(0,15)
-        console.log(values)
-    }
-    getAllCountries()
-
-
- 
+        
+       getAllLaunguage()
+  
+  
 
 
 }
@@ -80,19 +85,12 @@ getData();
 
 
 
+// const bhasha = item.languages.reduce((acc, curr) => {
+//   if (acc[curr.name]) acc[curr.name] == ++acc[curr.name];
+//   else acc[curr.name] = 1;
+//   return acc;
+// }, {});
 
-// const getAllLaunguage = () => {
-    //         recdata.map((item, index) => {
-    //             const bhasha = item.languages.map((language) => language.name)
-    //             arr2.push(bhasha)
-    //         });
-    //         const AllLanguage = (arr2.join(",").toString(""))
-    //         console.log(AllLanguage)
-    //         // const getFinal = () => {
-    //         //     const final =   AllLanguage.map((item, index)=>getLanguage(item))
-    //         //            console.log('final',final)
-    //         //    }
-    //         //    getFinal()
-    //     }
-        
-    //    console.log(getAllLaunguage()) 
+
+
+
